b03a6ad15a6aa48233402f602553d7c75824d44d
Habidhossen <habidhossen2@gmail.com>
2022-11-10 13:50:17 +0600

    tour model created

--- models/tour.model.js
+++ models/tour.model.js
@@ -1,5 +1,41 @@
 const mongoose = require("mongoose");
 
-const tourSchema = mongoose.Schema({});
+const tourSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: [true, "The {VALUE} already taken"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide a description for this tour!"],
+      trim: true,
+      maxLength: [50, "Description length must be {VALUE} characters"],
+    },
+    image: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: [true, "Please provide cost for this tour!"],
+      min: [0, "Price can't be negative"],
+    },
+    reviews: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("tour", tourSchema);
+module.exports = mongoose.model("Tour", tourSchema);
